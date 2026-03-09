3aa02f37a2df8c80a5b0fad18851adde2d8c801c
Vårin Sørlie <varinsor@gmail.com>
2026-03-09 22:13:14 +0100

    router changes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./styles/index.css";
 import { HashRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
- <HashRouter>
-  <App />
-</HashRouter>
+   <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
