009a1a48a5d9f99919276a22739697e273bf6a18
Vårin Sørlie <varinsor@gmail.com>
2026-03-10 19:13:22 +0100

    change to varinsorlie.github.no

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/varinsorlie/",
-  plugins: [react(), tailwindcss()],
+  base: "/",
+  plugins: [react()],
   resolve: {
     alias: {
       react: path.resolve(__dirname, "node_modules/react"),
       "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
-      // optional if problems persist:
-      // "react/jsx-runtime": path.resolve(__dirname, "node_modules/react/jsx-runtime"),
     },
   },
 });
-
 
 
 
