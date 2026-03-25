2efce019fb3e555a39e0163a3cdcb6478a6107c0
Vårin Sørlie <varinsor@gmail.com>
2026-03-25 09:34:45 +0100

    update backend files

--- backend/server.ts
+++ backend/server.ts
@@ -4,7 +4,7 @@ import { connectDB } from "./db.ts";
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     origin: "https://varinsorlie.github.io",
